1786a47f395fa44516d89d569c96413e48ae9529
Tiffany Kalin <tkalin@untangle.com>
2020-06-17 19:18:53 +0000

    Archive ALU operations until needed

---
+++ src/future/common.h
@@ -0,0 +1,12 @@
+#ifndef COMMON_H
+#define COMMON_H
+#include <stdint.h>
+#include "../imr.h"
+
+unsigned int imr_regs_needed(unsigned int len);
+int imr_register_get(const struct imr_state *s, uint32_t len);
+int bpf_reg_width(unsigned int len);
+int imr_register_alloc(struct imr_state *s, uint32_t len); 
+void imr_register_release(struct imr_state *s, uint32_t len); 
+
+#endif 
